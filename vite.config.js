f4597ea8222317115994ae3a0332e345496d8e71
Sone <sorndev89@gmail.com>
2024-07-05 19:38:40 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   build: {
     lib: {
       entry: './src/index.js',
